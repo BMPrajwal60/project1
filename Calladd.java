c94bd3fb12d9bfa491a59ebae1f3c1e1cf191d84
BMPrajwal60 <112654736+BMPrajwal60@users.noreply.github.com>
2022-09-02 00:28:47 +0530

    Add two number

---
+++ Calladd.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Calladd
+{
+public static void main(String args[])
+{
+float a,b,res;
+char chioce,ch;
+Scanner scan = new Scanner(System.in);
+
+do
+{
+System.out.println("1.  add two Numbers");
+System.out.println("2.  exit");
+System.out.println("Enter the chioce");
+chioce = scan.next().charAt(0);
+switch(chioce)
+{
+case'1'  : System.out.println("Enter the two numbers:");
+           a= scan.nextFloat();
+           b= scan.nextFloat();
+           res=a+b;
+           System.out.println("Result="+res);
+           break;
+case'2'  : System.exit(0);
+           break;
+
+default  : System.out.println("Invaild Choice");
+           break;
+}
+System.out.print("\n-----------------------------------\n");
+
+}
+while(chioce!=2);
+}
+}
